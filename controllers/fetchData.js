c29cae1272ee4f7c2a0754d7b9cce3c38a7cd9a7
saif <kh.s.k.rohan@gmail.com>
2024-11-09 13:05:21 +0600

    fr

---
+++ controllers/fetchData.js
@@ -0,0 +1,43 @@
+const connection = require('./dbConnection.js');
+const { scrapeStartech } = require('./scrapeMonitor.js');
+
+exports.fetchMonitorData=async(req,res)=>{
+    try{
+        scrapeStartech();
+        let [rows]=await connection.execute('SELECT * FROM monitor_shop INNER JOIN monitor_details ON monitor_shop.productName = monitor_details.productName;');
+        res.json({success:true,monitors:rows});
+    }catch(error){
+        console.error(error);
+        res.json({success:false});
+    }
+}
+
+exports.fetchCPUData=async(req,res)=>{
+    try{
+        let [rows]=await connection.execute('SELECT * FROM cpu_shop INNER JOIN cpu_details ON cpu_shop.productName = cpu_details.productName;');
+        res.json({success:true,cpus:rows});
+    }catch(error){
+        console.error(error);
+        res.json({success:false});
+    }
+}
+
+exports.fetchGPUData=async(req,res)=>{
+    try{
+        let [rows]=await connection.execute('SELECT * FROM gpu_shop INNER JOIN gpu_details ON gpu_shop.productName = gpu_details.productName;');
+        res.json({success:true,gpus:rows});
+    }catch(error){
+        console.error(error);
+        res.json({success:false});
+    }
+}
+
+exports.fetchRAMData=async(req,res)=>{
+    try{
+        let [rows]=await connection.execute('SELECT * FROM ram_shop INNER JOIN ram_details ON ram_shop.productName = ram_details.productName;');
+        res.json({success:true,rams:rows});
+    }catch(error){
+        console.error(error);
+        res.json({success:false});
+    }
+}
